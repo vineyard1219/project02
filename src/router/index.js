cfaf40dd6549561b95d24e898ffd04d3da21f715
vineyard1219 <skrud1219@naver.com>
2023-07-06 21:28:28 +0900

    main.js에 vuetify 적용?

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import CommunityNews from '../views/CommunityNews.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +10,14 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+
+    {
+      path: '/community-news',
+      name: 'community-news',
+      component: CommunityNews
     }
+
   ]
 })
 

79ee0de5e0a85360cf528c316da30c1eef014f71
vineyard1219 <skrud1219@naver.com>
2023-07-06 21:29:31 +0900

    main.js에 뷰티파이 적용

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify';
 
 import App from './App.vue'
 import router from './router'
+import Header from './components/Header.vue'
 
 const app = createApp(App)
 
+const vuetify = createVuetify()
+
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
+
+app.component('Header', Header) // Header 컴포넌트를 전역으로 등록합니다.
 
 app.mount('#app')

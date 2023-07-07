308a83e58484bbabf9228b9d9db65c80edc49f73
vineyard1219 <skrud1219@naver.com>
2023-07-07 15:46:35 +0900

    커뮤니티 연결

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
-import CommunityNews from '../views/CommunityNews.vue'
+import CommunityFree from '../views/CommunityFree.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,9 +13,9 @@ const router = createRouter({
     },
 
     {
-      path: '/community-news',
-      name: 'community-news',
-      component: CommunityNews
+      path: '/CommunityFree',
+      name: 'CommunityFree',
+      component: CommunityFree
     }
 
   ]
